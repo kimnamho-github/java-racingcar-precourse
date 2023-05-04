659ce411adefc842ddcdf5cd48e55fdf3e33df93
namho kim <skagh9697@gmail.com>
2023-05-04 22:05:30 +0900

    first commit

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +12,26 @@ public class Car {
     }
 
     // 추가 기능 구현
+
+//    public Car(String name, int position){
+//        this.name= name;
+//        this.position= position;
+//    }
+
+//    public String getName() {
+//        return name;
+//    }
+
+    public int getPosition(){
+        return position;
+    }
+    public void move(){
+        int random = Randoms.pickNumberInRange(0,9);
+
+        if(random >= 4){
+            position++;
+        }
+
+    }
+
 }
